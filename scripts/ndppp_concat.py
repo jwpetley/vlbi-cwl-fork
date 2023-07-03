60853cfaf5022830975660e4fa0d63a5471671ab
jwpetley <jwpetley@gmail.com>
2023-07-03 16:15:10 +0200

    Begun work on concat flow

---
+++ scripts/ndppp_concat.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -* coding: utf-8 -*-
+
+"""
+Create NDPPP concat parset based on input catalogue
+
+Created 7 July 2023
+
+@author: James Petley (jwpetley@gmail.com) 
+(Based on code from Leah Morabito)
+"""
+
+import argparse
+import numpy as np
+import os
+import logging
+from astropy.io import ascii
+import datetime
+from glob import glob
+
+from sort_times_into_freqGroups import main as sort_times_into_freqGroups
+import sys 
+import json 
+
+def main(cat_file):
+    mycat = ascii.read(cat_file)
+    ralist = np.array(mycat['RA'])
+    declist = np.array(mycat['DEC'])
+    srclist = np.array(mycat['Source_id'])
+
+    for i in range(0,len(srclist)):
+        # Find all MS for that src
+        mslist = glob('*'+str(srclist[i])+'*')
+        # Sort times into freq groups
+
+        output = sort_times_into_freqGroups(mslist)
+
+        filenames = output['filenames']
+        groupnames = output['groupnames']
+
+    print(filenames)
+    print(groupnames)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Create NDPPP concat parset based on input catalogue')
+    parser.add_argument('cat_file', type=str, help='Input catalogue file')
+    args = parser.parse_args()
+    main(args.cat_file)
+
+
