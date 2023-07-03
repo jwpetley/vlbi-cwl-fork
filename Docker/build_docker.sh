b49a6bdc2165a0e2a2dc7376b5ade8ec37c453cd
James Petley <53266085+jwpetley@users.noreply.github.com>
2023-07-03 10:50:20 +0200

    Initialising

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -5,3 +5,10 @@ DOCKER_TAG=latest
 
 docker build ${PWD} -f Docker/Dockerfile-vlbi -t vlbi-cwl:${DOCKER_TAG} #lofareosc/prefactor3-cwl:${DOCKER_TAG}
 #docker push lofareosc/prefactor3-cwl:${DOCKER_TAG}
+#! /bin/bash
+set -e
+
+DOCKER_TAG=latest
+
+docker build ${PWD} -f Docker/Dockerfile-vlbi -t vlbi-cwl:${DOCKER_TAG} #lofareosc/prefactor3-cwl:${DOCKER_TAG}
+#docker push lofareosc/prefactor3-cwl:${DOCKER_TAG}
