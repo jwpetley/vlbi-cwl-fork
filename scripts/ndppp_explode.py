88c5b78db1b860de8c5eab0a29865bbaf46a9d04
jwpetley <jwpetley@gmail.com>
2023-07-03 15:07:03 +0200

    Use delay_sols path later

--- scripts/ndppp_explode.py
+++ scripts/ndppp_explode.py
@@ -57,7 +57,7 @@ def main( msin, cat_file, delay_solset, phaseup_cmd="{ST001:'CS*'}", flag_cmd=''
         # Apply solutions and more average_target
 
         f.write( 'appylcal.type = applycal\n')
-        f.write( 'applycal.parmdb = {:s}\n'.format(str(delay_solset)) )
+        #f.write( 'applycal.parmdb = {:s}\n'.format(str(delay_solset)) )
         f.write( 'applycal.correction = fulljones\n')
         f.write( 'applycal.soltab = [amplitude000,phase000]\n')
 
