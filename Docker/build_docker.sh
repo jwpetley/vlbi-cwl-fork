9149268e9cd60b31b2911cdafa1d4f09cf8eb7ed
jwpetley <jwpetley@gmail.com>
2023-08-02 15:23:17 +0100

    Fixing duplication

--- Docker/build_docker.sh
+++ Docker/build_docker.sh
@@ -5,10 +5,3 @@ DOCKER_TAG=latest
 
 docker build ${PWD} -f Docker/Dockerfile-vlbi -t vlbi-cwl:${DOCKER_TAG} #lofareosc/prefactor3-cwl:${DOCKER_TAG}
 #docker push lofareosc/prefactor3-cwl:${DOCKER_TAG}
-#! /bin/bash
-set -e
-
-DOCKER_TAG=latest
-
-docker build ${PWD} -f Docker/Dockerfile-vlbi -t vlbi-cwl:${DOCKER_TAG} #lofareosc/prefactor3-cwl:${DOCKER_TAG}
-#docker push lofareosc/prefactor3-cwl:${DOCKER_TAG}
